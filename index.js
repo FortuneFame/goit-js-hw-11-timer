88a23ef477142b9e2a8b969e0b0014e2367f37f9
FortuneFame <fortunefame1710@gmail.com>
2021-01-14 00:02:26 +0200

    Добавила исходные файлы и сделала ТЗ

---
+++ index.js
@@ -0,0 +1,34 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+const targetDate = new Date("Jan 21, 2021");
+const countdownTimer = {
+  start() {
+    setInterval(() => {
+      const deltaTime = targetDate.getTime() - Date.now();
+      updateClockFace(deltaTime);
+    }, 1000);
+  },
+};
+
+countdownTimer.start();
+
+function updateClockFace(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
